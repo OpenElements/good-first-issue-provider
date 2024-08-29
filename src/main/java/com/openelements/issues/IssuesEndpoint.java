b956639e983180514527e91a0a137e06ff82b45c
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-08-29 11:14:26 +0200

    logging

--- src/main/java/com/openelements/issues/IssuesEndpoint.java
+++ src/main/java/com/openelements/issues/IssuesEndpoint.java
@@ -3,11 +3,14 @@ package com.openelements.issues;
 import java.util.List;
 import java.util.Objects;
 import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IssuesEndpoint {
+
+    private final static Logger log = org.slf4j.LoggerFactory.getLogger(IssuesEndpoint.class);
 
     private final IssueCache issueCache;
 
@@ -21,11 +24,13 @@ public class IssuesEndpoint {
 
     @GetMapping("/good-first-issues")
     public List<Issue> getGoodFirstIssues() {
+        log.info("Getting good first issues");
         return issueCache.getIssues(GOOD_FIRST_ISSUE);
     }
 
     @GetMapping("/good-first-issue-candidates")
     public List<Issue> getGoodFirstIssuesCandidates() {
+        log.info("Getting good first issue candidates");
         return issueCache.getIssues(GOOD_FIRST_ISSUE_CANDIDATE);
     }
 }
